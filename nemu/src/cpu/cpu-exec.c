d2fb3246fde028ab5ff745849bef199441c18fd4
24011211232-Deng Xidian <1447762756@qq.com>
2025-03-29 09:44:28 +0800

    commit032901

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -42,7 +42,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
-  // for(int i = 0; i < 32;i++){
+  for(int i = 0; i < 32;i++){
     
     if(watchpoint_diff(i) == true)
     {
