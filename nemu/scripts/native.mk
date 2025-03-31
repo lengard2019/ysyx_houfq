494e13795ac2a8090101ecb84186d9098cfe4145
pa1 <tracer@ysyx.org>
2025-03-31 13:39:36 +0800

    > compile NEMU ysyx_22040000 张三 Linux dengzibin-NBM-WXX9 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:39:36 up  3:09,  1 user,  load average: 0.36, 0.36, 0.33

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -21,7 +21,7 @@ include $(NEMU_HOME)/tools/difftest.mk
 compile_git:
 	$(call git_commit, "compile NEMU")
 $(BINARY):: compile_git
-$(BINARY_GDB):: compile_git
+
 # Some convenient rules
 
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
@@ -30,7 +30,7 @@ override ARGS += $(ARGS_DIFF)
 # Command to execute NEMU
 IMG ?=
 NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
-NEMU_EXEC_GDB := $(BINARY) $(ARGS) $(IMG)
+NEMU_EXEC_GDB := $(BINARY_GDB) $(ARGS) $(IMG)
 
 run-env: $(BINARY) $(DIFF_REF_SO)
 
