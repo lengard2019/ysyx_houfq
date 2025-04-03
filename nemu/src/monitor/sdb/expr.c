7df22534fdf76382d11bff1203e622cf1230047d
pa1 <tracer@ysyx.org>
2025-04-03 20:45:32 +0800

    > compile NEMU ysyx_22040000 张三 Linux dengzibin-NBM-WXX9 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:45:32 up 3 days, 10:14,  1 user,  load average: 0.75, 0.55, 0.49

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -668,7 +668,7 @@ static void gen_num()
 
 static void gen_rand_op()
 {
-  if(token_count >= MAX_TOKENS - 1){
+  if(token_count >= MAX_TOKENS - 3){
     return;
   }
 	char op[3] = {'+', '-', '*'};
