09f942944b1e3af291d9fd31e4dc220c85740434
pa1 <tracer@ysyx.org>
2025-04-03 14:26:29 +0800

    > compile NEMU ysyx_22040000 张三 Linux dengzibin-NBM-WXX9 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:26:29 up 3 days,  3:55,  1 user,  load average: 0.33, 0.40, 0.49

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -463,7 +463,7 @@ uint32_t eval(int p, int q) {
           while(check_parentheses(a,i) != true){
             i++;
           }
-          i++;
+          // i++;
         }
       if(!flag && tokens[i].type == LEQ)
         {
@@ -561,10 +561,10 @@ word_t expr(char *e, bool *success) {
 
   token_special();//特殊情况
   word_t result = 0;
-  // printf("%d\n", nr_token);
-  // for (int i = 0; i < nr_token; i++){
-  //   printf("%d %s\n",tokens[i].type, tokens[i].str);
-  // }
+  printf("%d\n", nr_token);
+  for (int i = 0; i < nr_token; i++){
+    printf("%d %s\n",tokens[i].type, tokens[i].str);
+  }
 
   if(check_parentheses(0, nr_token - 1) == false){
     printf("wrong parentheses used\n");
